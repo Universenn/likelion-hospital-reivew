435505ae98c583856c53fdf52dbf9f8d46fb90f8
Universenn <dnwndhks98@gmail.com>
2022-12-08 14:40:32 +0900

    first commit

---
+++ src/main/java/com/example/likelionhospitalreview/LikelionHospitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionhospitalreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionHospitalReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionHospitalReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionhospitalreview/LikelionHospitalReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionhospitalreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionHospitalReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
